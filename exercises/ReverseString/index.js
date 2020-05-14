900c8216a92f6701c77e453f30067f573bf40e73
LenWinkler <lenwinkler86@gmail.com>
2020-05-14 01:53:52 -0400

    finished ReverseString problem

--- exercises/ReverseString/index.js
+++ exercises/ReverseString/index.js
@@ -11,6 +11,19 @@
  */
 
 function reverse(str) {
+    /**
+     * Iterate over the string, starting at last char
+     * append chars to return str
+     * return the finished str
+     */
+
+    let reversed_str = '';
+
+    for (var i = str.length - 1; i > -1; i--) {
+        reversed_str += str[i]
+    };
+
+    return reversed_str;
 
 }
 
