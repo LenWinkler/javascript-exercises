07af2332e4c252f24ebe6c8fb0f6ddc3ed9304fe
LenWinkler <lenwinkler86@gmail.com>
2020-05-20 13:50:48 -0400

    finished Vowels problem

--- exercises/Vowels/index.js
+++ exercises/Vowels/index.js
@@ -10,6 +10,21 @@
  */
 
 function vowels(string) {
+    // use counter var
+    // iterate over string, incrementing counter for each vowel
+    // return counter
+
+    let counter = 0;
+    let vowels = 'aeiou';
+    let vowelList = new Set(vowels);
+
+    for (let i = 0; i < string.length; i++) {
+        if (vowelList.has(string[i].toLowerCase())) {
+            counter++;
+        }
+    };
+
+    return counter;
 
 }
 
