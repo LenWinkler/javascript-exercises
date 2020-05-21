a449970fa8245c8f5b550a7596686bdeda81ab09
LenWinkler <lenwinkler86@gmail.com>
2020-05-21 12:22:08 -0400

    finished Stack problem

--- exercises/Stack/index.js
+++ exercises/Stack/index.js
@@ -15,6 +15,21 @@
  */
 
 class Stack {
+    constructor() {
+        this.stack = []
+    }
+
+    push(element) {
+        this.stack.push(element)
+    }
+
+    pop() {
+        return this.stack.pop()
+    }
+
+    peek() {
+        return this.stack[this.stack.length - 1]
+    }
 
 }
 
